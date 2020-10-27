4bf457c80a91c54a89f9deacf9d9ded4c2d3489f
Dan1k0v <73443207+Dan1k0v@users.noreply.github.com>
2020-10-27 20:13:44 +0300

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,36 @@
+import time
+
+def wait(delay=3):
+    for i in range(delay):
+        print('.')
+        time.sleep(1)
+
+while True:
+    text = input('Put down some text ')
+    if text=='Hello':
+        wait()
+        print ('Hello.I\'m CatBot.')
+    elif text=='I\'m hungry':
+        wait()        
+        print ('I\'m too.')
+
+    elif text=='Are you a human?':
+        wait()
+        print ('I\'m not human.')
+    elif text=='Exit':
+        wait()
+        print ('Goodbye')
+        exit (0)
+    elif text=='Do you speak to Russian?':
+        wait()
+        print ('No')
+    elif text=='Who you?':
+        wait()
+        print ('I\'m a program.')
+    elif text=='Do you speak to English?':
+        wait()
+        print ('Yes')
+    else :
+        wait()
+        print('I don\'t understand.')
+    
